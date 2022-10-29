d613405f25954519ed8b06b0dd8f4514b4a48365
Tanita <tanita.pak@gmail.com>
2022-10-29 13:25:49 +0600

    создала модель для товара

--- app/webapp/admin.py
+++ app/webapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from webapp.models import Product
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'category']
+
+
+admin.site.register(Product)
--- app/webapp/models.py
+++ app/webapp/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.TextChoices):
+    ANY = 'AN', 'Разное'
+    COMPUTERS = 'CP', 'Компьютеры'
+    SMARTPHONES = 'SM', 'Смартфоны'
+    CLOTHES = 'CL', 'Одежда'
+
+
+class Product(models.Model):
+    name = models.CharField(
+        max_length=100,
+        null=False,
+        blank=False,
+        verbose_name="Название товара"
+    )
+    category = models.CharField(
+        max_length=2,
+        null=False,
+        blank=False,
+        choices=Category.choices,
+        default=Category.ANY
+    )
+    description = models.TextField(
+        max_length=3000,
+        null=True,
+        blank=True,
+        verbose_name='Описание товара'
+    )
+    image = models.ImageField(upload_to='images', null=True)
+
+
