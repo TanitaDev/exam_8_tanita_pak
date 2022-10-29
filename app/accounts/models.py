dcc286c51dd8e049fbdde2a918bfe3b0f8574d74
Tanita <tanita.pak@gmail.com>
2022-10-29 17:37:31 +0600

    добавила модель для профиля поьзователя

---
+++ app/accounts/models.py
@@ -0,0 +1,15 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(get_user_model(), related_name='profile', on_delete=models.CASCADE, verbose_name='Пользователь')
+    birthday = models.DateField(null=True, blank=True, verbose_name='Дата рождения')
+    avatar = models.ImageField(null=True, blank=True, upload_to='user_pics', verbose_name='Аватар')
+
+    def __str__(self):
+        return "Профиль" + " " + self.user.get_full_name()
+
+    class Meta:
+        verbose_name = 'Профиль'
+        verbose_name_plural = 'Профили'
